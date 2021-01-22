f54f9a4a09193853ec7a2ea7d8e621d4920c09f5
yesabhuiya1001 <yesabhuiya1001@gmail.com>
2021-01-22 10:51:15 -0600

    four paroblem solved

--- assignment.js
+++ assignment.js
@@ -10,6 +10,24 @@ var result = kilometerToMeter(5);
 console.log(result)
 
 
+// Budget calculator for Watch, Mobile and Laptop: Total code done by myself.
+var perWatch = 50;
+var perMobile = 100;
+var perLaptop = 500;
+function budgetCaculator(watch, mobile, laptop){
+  var budget = 0;
+  if(watch, mobile, laptop >= 0){
+    var watchCost = watch * 50;
+    var mobileCost = mobile * 100;
+    var laptopCost = laptop * 500;
+    budget = watchCost + mobileCost + laptopCost;
+  }
+  return budget;
+}
+var totalBudget = budgetCaculator(0,0,2);
+console.log(totalBudget)
+
+
 // find out the cost of a hotel when first ten days cost 100/day and after ten days the cost will reduce 20%: Copied the structure from practice module.
 function hotelCost(days){
     var cost = 0;
