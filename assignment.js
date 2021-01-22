d9467f5174020cebf4dd79f504196e8e6e322b6f
yesabhuiya1001 <yesabhuiya1001@gmail.com>
2021-01-22 08:49:18 -0600

    largest string find out done

--- assignment.js
+++ assignment.js
@@ -1,5 +1,7 @@
 // https://github.com/yesabhuiya1001/JS-assignment1
 
+
+// Kilometer to Meter.
 function kilometerToMeter(kilometer){
     var meter = kilometer * 1000;
     return meter;
@@ -8,7 +10,7 @@ var result = kilometerToMeter(5);
 console.log(result)
 
 
-
+// find out the cost of a hotel when first ten days cost 100/day and after ten days the cost will reduce 20%: Copied the structure from practice module.
 function hotelCost(days){
     var cost = 0;
     if(days<=10){
@@ -22,3 +24,17 @@ function hotelCost(days){
 var totalCost = hotelCost(11);
 console.log(totalCost)
 
+// find the leargest string from an array: Copied the structure from internet.
+var names = ["yesa", "musa", "ranajoy"]
+function findLongestWord(names) {
+    var longest = [0];
+
+  for(var i = 0; i < names.length; i++) {
+     if(names[i].length > longest.length) {
+       longest = names[i];
+        } 
+     }
+  return longest;
+}
+var largest = findLongestWord(names);
+console.log(largest)
