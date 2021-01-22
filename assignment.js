8e30cdb7d387dd626779094f673ccde6ca90eb1d
yesabhuiya1001 <yesabhuiya1001@gmail.com>
2021-01-22 03:46:27 -0600

    hotelCost done

--- assignment.js
+++ assignment.js
@@ -1,3 +1,4 @@
+// https://github.com/yesabhuiya1001/JS-assignment1
 
 function kilometerToMeter(kilometer){
     var meter = kilometer * 1000;
@@ -5,3 +6,19 @@ function kilometerToMeter(kilometer){
 }
 var result = kilometerToMeter(5);
 console.log(result)
+
+
+
+function hotelCost(days){
+    var cost = 0;
+    if(days<=10){
+        cost = days * 100;
+    }
+    else if(days>10){
+        cost = days * 80;
+    }
+    return cost;
+}
+var totalCost = hotelCost(11);
+console.log(totalCost)
+
